09f7c0310ab8b094edab625c4cd41dad01872f04
Rosie <r.gollancz@gmail.com>
2016-10-11 15:57:45 +0100

    Adds spec folder and first test, index and play files in features

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+source 'https://rubygems.org'
+
 require 'sinatra'
 require 'rspec-sinatra'
 require 'capybara'
---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+feature "Enter names" do
+      scenario "Entering 2 player names" do
+      visit "/"
+
+      fill_in :player1, with: 'George'
+      fill_in :player2, with: 'Zippy'
+      click_button "Battle commence!"
+      expect(page).to have_content('George vs. Zippy')
+      end
+    end
