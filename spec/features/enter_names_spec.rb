be44f2e4b6a384fbbb0aca4dbd99e16d56ce1f7e
Rosie <r.gollancz@gmail.com>
2016-10-11 18:11:29 +0100

    Adds Hit Points test and code in Play to pass

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature "Enter names" do
+  feature "Enter names" do
       scenario "Entering 2 player names" do
       visit("/")
 
@@ -10,3 +10,14 @@ feature "Enter names" do
       expect(page).to have_content('George vs. Zippy')
       end
     end
+
+    feature "Hit points" do
+        scenario "Player 1 viewing Player 2's hits points" do
+        visit("/")
+
+        fill_in :player1, with: 'George'
+        fill_in :player2, with: 'Zippy'
+        click_button "Battle commence!"
+        expect(page).to have_content('Zippy: 100 Hit Points')
+        end
+      end
